65c822f465a14db47667b56b8cca3312b2701e80
HankStat <hank96015@gmail.com>
2025-05-30 15:11:03 +0800

    fix: remove multiply function

--- Demo-Project/math_utils.py
+++ Demo-Project/math_utils.py
@@ -4,8 +4,6 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
-def multiply(x, y):
-    return x * y
 
 def pad_number(x):
     """Pad a number with leading zeros to ensure it has at least 'length' digits."""
